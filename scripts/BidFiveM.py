543fd9001f1bf63087f21f1452c48896ffdcda8b
nightluo <1872194982@qq.com>
2025-05-31 14:02:30 +0800

    Update BidFiveM.py

--- scripts/BidFiveM.py
+++ scripts/BidFiveM.py
@@ -178,7 +178,7 @@ def lambda_handler(event, context):
         utc_now = datetime.now(timezone.utc)
         beijing_time = utc_now.astimezone(timezone(timedelta(hours=8)))        
         end_time = beijing_time + timedelta(minutes=10)
-        send_test = webhook.send_text(f"重启，必胜！\n {start_time}")
+        send_test = webhook.send_text(f"重启，必胜！\n {beijing_time}")
         
         keyword_list = ["培训", "竞赛", "赋能", "会务", "交流活动", "辅助服务"]
         bid_total = []
