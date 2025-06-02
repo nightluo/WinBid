a5f950449427baae705ec4eb8a059fbdb622231f
nightluo <1872194982@qq.com>
2025-06-02 11:13:09 +0800

    Update BidFiveM.py

--- scripts/BidFiveM.py
+++ scripts/BidFiveM.py
@@ -236,6 +236,8 @@ def lambda_handler(event, context):
     
     except Exception as e:
         logger.error(f"全局异常: {str(e)}")
+        webhook = WeComWebhook()
+        error_send = webhook.send_text(f"全局异常: {str(e)}")
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
