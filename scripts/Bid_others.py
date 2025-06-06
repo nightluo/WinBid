e7fc1a1b10f51baf794541926daabce3739453d7
nightluo <1872194982@qq.com>
2025-06-06 12:35:04 +0800

    Update Bid_others.py

--- scripts/Bid_others.py
+++ scripts/Bid_others.py
@@ -12,9 +12,10 @@ from logging.handlers import RotatingFileHandler
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-# keyword_list = ["培训", "竞赛", "赋能", "会务", "营销"]
+keyword_main = ["培训", "竞赛", "赋能", "会务", "营销"]
 keyword_list = ["交流活动", "辅助服务", "训战", "会议", "会展", "论坛", "实战", "服务支撑", "服务提质", "客户价值提升", "训练营"]
 not_list = ["会议室", "会议终端设备", "会议系统", "租赁"]
+not_list = not_list.append(keyword_main)
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
